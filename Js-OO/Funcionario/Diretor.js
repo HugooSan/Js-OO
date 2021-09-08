6d60a20eec547bb60461c9cd60c25ffd252ad05f
Hugo Shinde <82584511+HugooSan@users.noreply.github.com>
2021-09-08 20:55:33 -0300

    first commit

---
+++ Js-OO/Funcionario/Diretor.js
@@ -0,0 +1,9 @@
+import { Funcionario } from "./Funcionario.js";
+
+
+export class Diretor {
+    constructor(nome, salario, cpf) {
+        super(nome, salario, cpf)
+        this._bonificacao = 2;
+    }
+}
---
+++ Js-OO/index.js
@@ -0,0 +1,7 @@
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+
+const diretor = new Diretor("Hugo", 1000000000, 23666999874);
+const gerente = new Gerente("dhuewq", hufweqpfhu, hufqorhuf);
+
+const estalogado = SistemaAutenticacao.login(diretor, 123456789);
