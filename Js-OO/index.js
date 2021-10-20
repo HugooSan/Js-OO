bf6667609f712c7fd4eba779d933f78d28212506
Hugo Shinde <82584511+HugooSan@users.noreply.github.com>
2021-10-20 15:16:56 -0300

    finalização do curso de Js OO

--- Js-OO/index.js
+++ Js-OO/index.js
@@ -1,7 +1,14 @@
+import { Cliente } from "./Cliente.js";
 import { Gerente } from "./Funcionario/Gerente.js";
 import { Diretor } from "./Funcionario/Diretor.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js"
+
 
 const diretor = new Diretor("Hugo", 1000000000, 23666999874);
-const gerente = new Gerente("dhuewq", hufweqpfhu, hufqorhuf);
+const gerente = new Gerente("dhuewq", 13215496, 1234987998);
+
+diretor.cadastrarSenha(123456789);
+gerente.cadastrarSenha(456321);
 
 const estalogado = SistemaAutenticacao.login(diretor, 123456789);
+console.log(estalogado);
